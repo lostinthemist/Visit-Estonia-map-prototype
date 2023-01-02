bcc516816ba5c2a329c2b973f8bd3f5bf62271b4
Rauno Kaldmaa <7577899+lostinthemist@users.noreply.github.com>
2023-01-02 02:35:45 +0200

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+// $(".green-link").click(function(e){
+//     event.preventDefault(e);
+//     $(this).remove();
+// // $("p").removeClass("intro");
+// });
+
+
+// $(document).on('click', '.green-link', function(event) { 
+//     console.log('came'); 
+//         $(".green-link").click(); 
+//     });
+    
+//     $(".green-link").click(function(){
+//         alert('click');
+//     });
